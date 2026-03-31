c50839cf173cf2ffbcc4499f1e221498e6254853
saran <saran.it22@bitsathy.ac.in>
2026-03-31 10:05:27 +0530

    AUTHENTICATION MODULES

---
+++ backend/eat_it/src/main/java/com/example/eat_it/user/repository/UserRepo.java
@@ -0,0 +1,15 @@
+package com.example.eat_it.user.repository;
+
+import com.example.eat_it.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+}
