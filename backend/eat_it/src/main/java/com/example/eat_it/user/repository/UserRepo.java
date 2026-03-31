e568ac15ce5f9bcf8f61340ce88fc0b750c3bd65
saran <saran.it22@bitsathy.ac.in>
2026-03-31 13:25:25 +0530

    updated user repo and config

--- backend/eat_it/src/main/java/com/example/eat_it/user/repository/UserRepo.java
+++ backend/eat_it/src/main/java/com/example/eat_it/user/repository/UserRepo.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 public interface UserRepo extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+    Optional<User> findByName(String name);
+    boolean existsByName(String username);
 
     boolean existsByEmail(String email);
 
